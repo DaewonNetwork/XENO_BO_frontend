88f42268bbb43f9ce587de994c483c22231684ba
huntingforheat <asktt1234@gmail.com>
2024-09-08 21:14:44 +0900

    컴포넌트 렌더링 수정

--- client/src/(FSD)/entities/manager/api/useGetAllBrand.ts
+++ client/src/(FSD)/entities/manager/api/useGetAllBrand.ts
@@ -10,5 +10,8 @@ export const useGetAllBrand = () => {
     return useQuery<BrandInfo[]>({
         queryKey: ALL_BRAND_QUERY_KEY,
         queryFn: () => fetchData({ path: "/manager/brand", isAuthRequired: true }),
+        staleTime: 0, // 데이터를 항상 "오래된" 것으로 간주
+        refetchInterval: 500, // 5초마다 백그라운드에서 새 데이터 확인
+        refetchOnWindowFocus: true, // 윈도우가 포커스를 받을 때 새 데이터 확인
     });
 };
